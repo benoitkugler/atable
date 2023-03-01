0b5d04f8a90787f19f7a91a0d377ab37b7c394e6
Benoit KUGLER <x.ben.x@free.fr>
2023-03-01 09:10:47 +0100

    titre appli web

--- server/static/shop/flutter_service_worker.js
+++ server/static/shop/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "manifest.json": "58e5c831c88b45a733ef6beee6940a2f",
 "main.dart.js": "8d7d3834efe26e369f0824dd111028fd",
-"index.html": "975b11de378d33d0f0af87ed08b79e9c",
-"/": "975b11de378d33d0f0af87ed08b79e9c",
+"index.html": "3f6482327a33ba1c6930a8864bbb2a83",
+"/": "3f6482327a33ba1c6930a8864bbb2a83",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
