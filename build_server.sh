6c3b95227c36605b38aa7fc5ec5a86c0c2860144
Benoit KUGLER <benoit.kugler@gmail.com>
2023-08-02 19:18:50 +0200

    build

---
+++ build_server.sh
@@ -0,0 +1,14 @@
+# Build script to execute from www/
+echo "Removing current folder"
+rm -rf atable/ &&
+git clone https://github.com/benoitkugler/atable.git && 
+echo "Removing unused files" && 
+rm -rf atable/.git atable/atable-web atable/atable-mobile &&
+echo "Moving into server/src" && 
+cd atable/server/src && 
+echo "Building executable" &&
+go build *.go && 
+echo "Cleaning cache" &&
+go clean -modcache && 
+rm -rf ../../../../.cache/go-build/ &&
+echo "Done."
