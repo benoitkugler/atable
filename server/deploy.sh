6547450c21a34da12fbdefb67414d9260798247e
Benoit KUGLER <benoit.kugler@gmail.com>
2024-04-16 15:59:50 +0200

    [server] build static executables

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,6 +1,6 @@
 echo "Building..."
 cd src/ && 
-go build -ldflags "-w" *.go  && 
+CGO_ENABLED=0 go build -ldflags "-w" *.go  && 
 echo "Deploying..." &&
 scp main intendance@ssh-intendance.alwaysdata.net:www/main &&
 echo "Cleaning up..." && 
