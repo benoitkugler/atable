876af6a71ea5c151f217b6c4b20537168d8fe634
Benoit KUGLER <benoit.kugler@gmail.com>
2024-08-02 18:00:10 +0200

    [mobile] bump flutter version and deps

--- atable-mobile/android/app/src/main/kotlin/com/example/atable/MainActivity.kt
+++ atable-mobile/android/app/src/main/kotlin/com/example/atable/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.intendance.atable
+package com.example.atable
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
