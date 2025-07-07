488e473aab5db41119252abca15f87e8d6e8944e
Benoit KUGLER <benoit.kugler@gmail.com>
2025-07-07 12:00:13 +0200

    fix script

--- build_server.sh
+++ build_server.sh
@@ -2,7 +2,7 @@
 # static files are pulled from git; go executable should be
 # uploaded after cross compilation
 #
-git clone https://github.com/benoitkugler/atable.git && 
+git pull && 
 echo "Removing unused files" && 
 # only keep server/static
 rm -rf atable/.git atable/atable-web atable/atable-mobile atable/server/src atable/server/migrations &&
