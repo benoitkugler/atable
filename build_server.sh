5a3a812ac94599b7d6c66c16b5dd7318e112de27
Benoit KUGLER <benoit.kugler@gmail.com>
2025-07-07 12:02:57 +0200

    fix script

--- build_server.sh
+++ build_server.sh
@@ -1,9 +1,9 @@
-# Build script to execute from www/
+# Build script to execute from atable
 # static files are pulled from git; go executable should be
 # uploaded after cross compilation
 #
 git pull && 
 echo "Removing unused files" && 
 # only keep server/static
-rm -rf atable/atable-web atable/atable-mobile atable/server/src atable/server/migrations &&
+rm -rf atable-web atable-mobile server/src server/migrations &&
 echo "Done (pending uploading executable to atable/server)."
