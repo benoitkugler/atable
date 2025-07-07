6957969e65c9d0be78ce07ae48003d44152cd4e5
Benoit KUGLER <benoit.kugler@gmail.com>
2025-07-07 11:24:47 +0200

    fix bug

--- build_server.sh
+++ build_server.sh
@@ -1,6 +1,6 @@
 # Build script to execute from www/
-echo "Removing current folder"
-rm -rf atable/ &&
+# echo "Removing current folder"
+# rm -rf atable/ &&
 git clone https://github.com/benoitkugler/atable.git && 
 echo "Removing unused files" && 
 rm -rf atable/.git atable/atable-web atable/atable-mobile &&
